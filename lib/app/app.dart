b14acd7704b391bc8dcc6f4e6c3262fcc6eb7db4
Edgar Gataullin <ERGataullin2000@gmail.com>
2023-10-08 20:57:53 +0300

    Localizations import change in app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 
 import '/app/dependencies_provider.dart';
+import '/app/localizations.dart';
 import '/app/router.dart';
 
 class App extends StatelessWidget {
