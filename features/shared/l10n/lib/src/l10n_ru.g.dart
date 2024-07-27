80c31eb3b9a9f94d22cdf62a3f27de1ff1240536
Edgar Gataullin <92092442+ERGataullin@users.noreply.github.com>
2024-07-27 18:49:58 +0300

    Player video quality (#98)

--- features/shared/l10n/lib/src/l10n_ru.g.dart
+++ features/shared/l10n/lib/src/l10n_ru.g.dart
@@ -87,6 +87,9 @@ class L10nRu extends L10n {
   String get popularTitle => 'Популярное';
 
   @override
+  String get qualityLabel => 'Качество';
+
+  @override
   String get signInSubmitLabel => 'Войти';
 
   @override
@@ -97,6 +100,11 @@ class L10nRu extends L10n {
 
   @override
   String get upNextTitle => 'К просмотру';
+
+  @override
+  String videoQuality(num quality) {
+    return '${quality}p';
+  }
 
   @override
   String videoQualityType(String type) {
