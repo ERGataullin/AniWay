252f35f582949c5c98d3fd8da252f55dcd0204ed
Edgar Gataullin <92092442+ERGataullin@users.noreply.github.com>
2024-07-29 22:22:09 +0300

    Movies player loading fix (#101)

--- features/common/movies/lib/src/data/sources/anime365.dart
+++ features/common/movies/lib/src/data/sources/anime365.dart
@@ -323,7 +323,7 @@ class Anime365MoviesDataSource implements MoviesDataSource {
       'tv_special' => MovieTypeDto.tvSpecial,
       'cm' => MovieTypeDto.ad,
       'music' => MovieTypeDto.music,
-      'pv' => MovieTypeDto.preview,
+      'preview' || 'pv' => MovieTypeDto.preview,
       final Object? unsupported => throw UnsupportedError(
           'Unsupported movie type: $unsupported',
         ),
