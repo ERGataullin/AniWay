b486c155704946e9f541b6cd8a92aaf6f8976978
Marat <Mar.shiga@mail.ru>
2024-11-23 20:50:36 +0300

    Movies details poster height fix (#145)
    Co-authored-by: KAPLMARKS <Mar.shiga@mail.com>

--- features/common/movies/lib/src/presentation/movie/widget.dart
+++ features/common/movies/lib/src/presentation/movie/widget.dart
@@ -110,9 +110,9 @@ class _AppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final Color surfaceColor = Theme.of(context).colorScheme.surface;
     return ListenableBuilder(
-      listenable: context.wm.showLoader,
+      listenable: context.wm.posterHeight,
       builder: (context, __) => SliverAppBar(
-        expandedHeight: context.wm.showLoader.value ? null : 400,
+        expandedHeight: context.wm.posterHeight.value,
         leading: IconButton.filledTonal(
           onPressed: Navigator.of(context).pop,
           icon: const Icon(Icons.arrow_back),
