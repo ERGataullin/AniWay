76ef43646af9843bb9f8c593106a765494e96fa2
Edgar Gataullin <ERGataullin2000@yandex.ru>
2025-02-13 23:04:08 +0300

    Core AdaptiveImageBuilder fix

--- lib/core/presentation/components/image/adaptive_image_builder.dart
+++ lib/core/presentation/components/image/adaptive_image_builder.dart
@@ -56,12 +56,18 @@ class _AdaptiveImageBuilderState extends State<AdaptiveImageBuilder> {
   }
 
   void _updateImage() {
-    final MapEntry<num, Uri> imageEntry =
-        widget.image!.resolutionsUris.entries.firstWhere(
-      (entry) => entry.key >= _breakpointWidth,
-      orElse: () => widget.image!.resolutionsUris.entries.last,
-    );
-    
+    if (widget.image == null) {
+      _imageProvider = null;
+      _imageWidth = -1;
+      return;
+    }
+
+    final MapEntry<num, Uri> imageEntry = widget.image!.resolutionsUris.entries
+        .firstWhere(
+          (entry) => entry.key >= _breakpointWidth,
+          orElse: () => widget.image!.resolutionsUris.entries.last,
+        );
+
     _imageProvider = NetworkImage(
       context
           .read<NetworkService>()
