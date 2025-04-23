4c889688e6d267f72c44a2da14534455c070e9b6
Edgar Gataullin <ERGataullin2000@yandex.ru>
2025-04-23 19:30:50 +0300

    Player loader fix

--- lib/player/utils/video_controller.dart
+++ lib/player/utils/video_controller.dart
@@ -83,12 +83,8 @@ class VideoController {
 
   Future<void> seekTo(Duration position) async {
     _assertHasInner();
-    final effectivePosition = Duration(
-      milliseconds: (position.inMilliseconds ~/ 100) * 100,
-    );
-    print('Log: Seek to $position');
-    this.position.value = effectivePosition;
-    await _inner.value?.seekTo(effectivePosition);
+    this.position.value = position;
+    await _inner.value?.seekTo(position);
   }
 
   Future<void> setPlaybackSpeed(double speed) async {
@@ -118,53 +114,30 @@ class VideoController {
     final VideoPlayerValue value =
         _inner.value?.value ?? const VideoPlayerValue.uninitialized();
 
-    const positionMeasurementError = Duration(seconds: 1);
-    final bool isPositionCorrect =
-        (position.value - value.position).abs() <= positionMeasurementError;
-    final bool isPositionBuffered = value.buffered.any(
-      (range) =>
-          range.start <= position.value + positionMeasurementError &&
-          position.value <= range.end + positionMeasurementError,
-    );
-    loading.value = switch (defaultTargetPlatform) {
-      _ when !value.isInitialized => true,
-      _ when value.isCompleted || value.hasError => false,
-      _ when kIsWeb => value.isBuffering,
-      TargetPlatform.linux || TargetPlatform.windows => value.isBuffering,
-      TargetPlatform.android ||
-      TargetPlatform.fuchsia ||
-      TargetPlatform.iOS ||
-      TargetPlatform.macOS =>
-        playing.value && (!isPositionCorrect || !isPositionBuffered),
-    };
-    // final Duration positionChangeMax = switch (defaultTargetPlatform) {
-    //   _ when kIsWeb => Duration.zero,
-    //   TargetPlatform.android ||
-    //   TargetPlatform.iOS => const Duration(seconds: 1),
-    //   TargetPlatform.fuchsia ||
-    //   TargetPlatform.linux ||
-    //   TargetPlatform.macOS ||
-    //   TargetPlatform.windows => Duration.zero,
-    // };
-    // final bool isPositionCorrect =
-    //     (position.value - value.position).abs() < positionChangeMax;
-    // final bool isPositionBuffered = value.buffered.any(
-    //   (range) =>
-    //       range.start <= position.value + positionChangeMax &&
-    //       position.value < range.end + positionChangeMax,
-    // );
-    // loading.value =
-    //     !value.isCompleted &&
-    //     !value.hasError &&
-    //     playing.value &&
-    //     (!isPositionCorrect || !isPositionBuffered);
-    print(
-      'Log: -----------------------------------------------------------------\n'
-      'Log: Loading: ${loading.value}\n'
-      'Log: Buffered: ${value.buffered}\n'
-      'Log: Position: ${position.value}\n'
-      'Log: Inner position: ${value.position}\n',
-    );
+    switch (defaultTargetPlatform) {
+      case _ when !value.isInitialized:
+        loading.value = true;
+      case _ when value.isCompleted || value.hasError:
+        loading.value = false;
+      case _ when kIsWeb:
+        loading.value = value.isBuffering;
+      case TargetPlatform.linux || TargetPlatform.windows:
+        loading.value = value.isBuffering;
+      case TargetPlatform.android ||
+          TargetPlatform.fuchsia ||
+          TargetPlatform.iOS ||
+          TargetPlatform.macOS:
+        const positionMeasurementError = Duration(seconds: 1);
+        final bool isPositionCorrect =
+            (position.value - value.position).abs() <= positionMeasurementError;
+        final bool isPositionBuffered = value.buffered.any(
+          (range) =>
+              range.start <= position.value + positionMeasurementError &&
+              position.value < range.end + positionMeasurementError,
+        );
+        loading.value =
+            playing.value && (!isPositionCorrect || !isPositionBuffered);
+    }
 
     aspectRatio.value = value.aspectRatio;
     if (value.isCompleted || value.hasError) playing.value = false;
