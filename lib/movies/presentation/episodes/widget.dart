c33ec260564a958e50a3b8e227c20dc1ec89c8ca
Edgar Gataullin <ERGataullin2000@yandex.ru>
2025-05-01 20:35:57 +0300

    Movies episodes adaptiveness

--- lib/movies/presentation/episodes/widget.dart
+++ lib/movies/presentation/episodes/widget.dart
@@ -2,6 +2,7 @@ import 'package:app/core/core.dart';
 import 'package:app/l10n/l10n.dart';
 import 'package:app/movies/presentation/components/episode_card.dart';
 import 'package:app/movies/presentation/episodes/wm.dart';
+import 'package:app/root_menu/root_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
 
@@ -25,31 +26,33 @@ class EpisodesWidget extends ElementaryWidget<IEpisodesWM> {
   Widget build(IEpisodesWM wm) {
     return Provider<IEpisodesWM>.value(
       value: wm,
-      child: ShimmerScope(
-        child: ListenableBuilder(
-          listenable: Listenable.merge([wm.tabController, wm.tabsTexts]),
-          builder:
-              (context, _) => Scaffold(
-                appBar: AppBar(
-                  title: Text(context.l10n.episodesLabel),
-                  bottom:
+      child: RootMenuAwaredCenter(
+        child: ShimmerScope(
+          child: ListenableBuilder(
+            listenable: Listenable.merge([wm.tabController, wm.tabsTexts]),
+            builder:
+                (context, _) => Scaffold(
+                  appBar: AppBar(
+                    title: Text(context.l10n.episodesLabel),
+                    bottom:
+                        wm.tabController.value == null
+                            ? null
+                            : TabBar(
+                              controller: wm.tabController.value,
+                              isScrollable: true,
+                              tabs: wm.tabsTexts.value
+                                  .map((text) => Tab(text: text))
+                                  .toList(growable: false),
+                            ),
+                  ),
+                  body:
                       wm.tabController.value == null
-                          ? null
-                          : TabBar(
-                            controller: wm.tabController.value,
-                            isScrollable: true,
-                            tabs: wm.tabsTexts.value
-                                .map((text) => Tab(text: text))
-                                .toList(growable: false),
-                          ),
+                          ? const Center(
+                            child: CircularProgressIndicator.adaptive(),
+                          )
+                          : const _Episodes(),
                 ),
-                body:
-                    wm.tabController.value == null
-                        ? const Center(
-                          child: CircularProgressIndicator.adaptive(),
-                        )
-                        : const _Episodes(),
-              ),
+          ),
         ),
       ),
     );
@@ -61,6 +64,7 @@ class _Episodes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double spacing = Breakpoint.activeBreakpointOf(context).padding;
     return ListenableBuilder(
       listenable: context.wm.tabsEpisodes,
       builder:
@@ -74,13 +78,12 @@ class _Episodes extends StatelessWidget {
                       Breakpoint.activeBreakpointOf(context).margin,
                     ),
                     itemCount: episodes.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithMaxCrossAxisExtent(
-                          childAspectRatio: 16 / 10,
-                          crossAxisSpacing: 8,
-                          mainAxisSpacing: 8,
-                          maxCrossAxisExtent: 128 + 64,
-                        ),
+                    gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      childAspectRatio: 16 / 10,
+                      crossAxisSpacing: spacing,
+                      mainAxisSpacing: spacing,
+                      maxCrossAxisExtent: 256,
+                    ),
                     itemBuilder:
                         (context, index) => EpisodeCard(
                           episodes[index],
